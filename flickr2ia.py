3134a637d80cc6377106070a13bd9564c0eb6eb6
Jake Johnson <jake@archive.org>
2012-06-08 21:57:42 +0000

    only generate metadata, don't move or rename original filesgit add flickr2ia.py

--- flickr2ia.py
+++ flickr2ia.py
@@ -4,10 +4,7 @@ import os
 import yaml
 import simplejson as json
 import datetime
-from collections import defaultdict
-
-ROOT_DIR = os.getcwd()
-PHOTO_DIR = os.path.join(ROOT_DIR, 'nasahqphoto')
+from lxml import etree
 
 
 #______________________________________________________________________________
@@ -16,7 +13,7 @@ def get_coverage(location_dict):
     if location.get('latitude'):
         coverage = "%s %s" % (location['latitude'], location['longitude'])
     if location.get('neighbourhood'):
-        if coverage: coverage = coverage + '; ' + location['neighbourhood']['_content']
+        if coverage: coverage = coverage + ', ' + location['neighbourhood']['_content']
         else: coverage = location['neighbourhood']['_content']
     if location.get('county'):
         if coverage: coverage = coverage + ', ' + location['county']['_content']
@@ -27,47 +24,61 @@ def get_coverage(location_dict):
     if location.get('country'):
         if coverage: coverage = coverage + ', ' + location['country']['_content']
         else: coverage = location['country']['_content']
-    print coverage
+    return coverage
 
 #______________________________________________________________________________
-def exists_on_ia(flickr_ids):
-    oldid = "%s_%s" % flickr_ids
-    possible_ids = ([oldid + "_o", oldid + "_b", oldid + "_z",
-                     "nasahqphoto-flickr-%s" % flickr_ids[0]])
-    for id in possible_ids:
-        meta_url = 'http://archive.org/metadata/%s' % id
-        j = json.loads(urllib2.urlopen(meta_url).read())
-        if j:
-            return True
-    return False
-
-#______________________________________________________________________________
-items = [x for x in os.listdir('nasahqphoto') if x.endswith('info.yaml')]
-for item in items:
-    info_file = os.path.join(PHOTO_DIR, item)
-    meta_dict = yaml.load(open(info_file).read())['photo']
-    flickr_ids = (meta_dict['id'], meta_dict['originalsecret'])
-    if exists_on_ia(flickr_ids):
-        continue
-    identifier = "nasa-flickr-%s" % meta_dict['id']
-    description = meta_dict['description']['_content']
-    date = meta_dict['dates']['taken'].strftime('%Y-%m-%d')
-    subject = '; '.join([x['raw'] for x in meta_dict['tags']['tag']])
-    source = meta_dict['urls']['url'][0]['_content']
-    title = meta_dict['title']
-    coverage = get_coverage(meta_dict['location'])
-
-    license_map = {"1": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
+def get_license(license_dict):
+    license_map = {"0": None,
+                   "1": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
                    "2": "http://creativecommons.org/licenses/by-nc/2.0/",
                    "3": "http://creativecommons.org/licenses/by-nc-nd/2.0/",
                    "4": "http://creativecommons.org/licenses/by/2.0/",
                    "5": "http://creativecommons.org/licenses/by-sa/2.0/",
                    "6": "http://creativecommons.org/licenses/by-nd/2.0/",
                    "7": "http://flickr.com/commons/usage/"}
-    licenseurl = license_map[meta_dict['license']]
-    print licenseurl
+    return license_map[license_dict]
 
+#______________________________________________________________________________
+def mk_metadata(meta_dict, ITEM_DIR):
+    fxml = os.path.join(ITEM_DIR, "%s_files.xml" % meta_dict['identifier'])
+    f = open(fxml, "wb")
+    f.write("<files />")
+    f.close()
+    root = etree.Element("metadata")
+    for k,v in meta_dict.iteritems():
+        subElement = etree.SubElement(root,k)
+        subElement.text = v
+    meta_xml = etree.tostring(root, pretty_print=True,
+                              xml_declaration=True, encoding="utf-8")
+    mxml = os.path.join(ITEM_DIR, "%s_meta.xml" % meta_dict['identifier'])
+    ff = open(mxml, "wb")
+    ff.write(meta_xml)
+    ff.close()
 
-    #print "\n\n----\n\n"
-    #for k,v in meta_dict.iteritems():
-    #    print "%s\t=>\t%s" % (k,v)
+#______________________________________________________________________________
+ROOT_DIR = os.getcwd()
+items = [x[0] for x in os.walk('nasahqphoto') if x[0] != 'nasahqphoto']
+for item in items:
+    identifier = item.split('/')[-1]
+    ITEM_DIR = os.path.join(ROOT_DIR, 'nasahqphoto', identifier)
+    print ITEM_DIR
+    info_file = os.path.join(ITEM_DIR, '%s_info.yaml' % identifier)
+    comments_file = info_file.replace('_info.yaml', '_comments.yaml')
+    exif_file = info_file.replace('_info.yaml', '_exif.yaml')
+    people_file = info_file.replace('_info.yaml', '_people.yaml')
+    tags_file = info_file.replace('_info.yaml', '_tags.yaml')
+    og_photo = info_file.replace('_info.yaml', '_jpg')
+    meta_dict = yaml.load(open(info_file).read())['photo']
+    ia_meta = dict(identifier = "nasahqphoto-%s" % meta_dict['id'],
+                   description = meta_dict['description']['_content'],
+                   date = meta_dict['dates']['taken'].strftime('%Y-%m-%d'),
+                   subject = '; '.join([x['raw'] for x in meta_dict['tags']['tag']]),
+                   source = meta_dict['urls']['url'][0]['_content'],
+                   title = meta_dict['title']['_content'],
+                   coverage = get_coverage(meta_dict['location']),
+                   license = get_license(meta_dict['license']),
+                   mediatype = 'image',
+                   collection = 'nasaheadquartersflickrstream',
+                   creator = 'NASA')
+    ia_meta = dict((k,v) for (k,v) in ia_meta.iteritems() if v)
+    mk_metadata(ia_meta, ITEM_DIR)
